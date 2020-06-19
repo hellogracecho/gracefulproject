2374d6e5a23b8aa03d48178b7ebb49002b230596
Grace Cho <48530228+hellogracecho@users.noreply.github.com>
2020-06-19 10:18:14 -0700

    food recipe build added

---
+++ food-recipe/precache-manifest.99961de32b351a366818c09e9c8d6ae7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e92db35c827e5f664af58474d9ab135e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "800379abdac6879652e3",
+    "url": "/static/css/main.d430ebc8.chunk.css"
+  },
+  {
+    "revision": "b5cae96bab4278e7cf71",
+    "url": "/static/js/2.5502684d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5502684d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "800379abdac6879652e3",
+    "url": "/static/js/main.72370d5e.chunk.js"
+  },
+  {
+    "revision": "1d92436f3fb13db5fb5d",
+    "url": "/static/js/runtime-main.13fec897.js"
+  }
+]);
