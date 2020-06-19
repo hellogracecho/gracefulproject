e82a32fc60d07f9f67de13c64c9d627cac63c3df
Grace Cho <48530228+hellogracecho@users.noreply.github.com>
2020-06-19 10:00:33 -0700

    Updates

---
+++ precache-manifest.e1d15c7e13716076688e9f3ef4b53de1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78fef81dba679d02e7d16b88e885106f",
+    "url": "/food-recipe/index.html"
+  },
+  {
+    "revision": "248e47783062aad3499f",
+    "url": "/food-recipe/static/css/main.d430ebc8.chunk.css"
+  },
+  {
+    "revision": "b5cae96bab4278e7cf71",
+    "url": "/food-recipe/static/js/2.5502684d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/food-recipe/static/js/2.5502684d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "248e47783062aad3499f",
+    "url": "/food-recipe/static/js/main.9fef439a.chunk.js"
+  },
+  {
+    "revision": "779335723206f8e70329",
+    "url": "/food-recipe/static/js/runtime-main.3dc38f30.js"
+  }
+]);
